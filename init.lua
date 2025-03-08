0822a17ce384ed85cd6c66f994b90fbca5500977
sztorma <sztorma@outlook.com>
2025-03-08 17:40:10 +0100

    lua and json lsp

--- init.lua
+++ init.lua
@@ -16,5 +16,6 @@ require('lazy').setup({
     require 'plugins.bufferline',
     require 'plugins.lualine',
     require 'plugins.treesitter',
-    require 'plugins.telescope'
+    require 'plugins.telescope',
+    require 'plugins.lsp'
 })
