ab352de133be0b13dbcba9c5b1a419fa1ac6d96e
sztorma <sztorma@outlook.com>
2025-03-17 11:37:51 +0100

    autoformat

--- init.lua
+++ init.lua
@@ -18,5 +18,6 @@ require('lazy').setup({
     require 'plugins.treesitter',
     require 'plugins.telescope',
     require 'plugins.lsp',
-    require 'plugins.autocompletion'
+    require 'plugins.autocompletion',
+    require 'plugins.none-lsp'
 })
