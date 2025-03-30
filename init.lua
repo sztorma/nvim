c782195d507147c3d8e45e1b442e8aec995e7405
sztorma <sztorma@outlook.com>
2025-03-30 12:17:01 +0200

    indent

--- init.lua
+++ init.lua
@@ -21,5 +21,6 @@ require('lazy').setup({
     require 'plugins.autocompletion',
     require 'plugins.none-lsp',
     require 'plugins.gitsigns',
-    require 'plugins.alpha'
+    require 'plugins.alpha',
+    require 'plugins.indent-blankline'
 })
