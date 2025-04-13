78377cf69e04d63d69ab5021cd94e37df3925a15
sztorma <sztorma@outlook.com>
2025-04-13 10:27:35 +0200

    autoclose parentheses

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -15,4 +15,11 @@ return {
 		-- Hints keybinds
 		"folke/which-key.nvim",
 	},
+	{
+		-- Autoclose parentheses, brackets, quotes, etc.
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		opts = {},
+	},
 }
