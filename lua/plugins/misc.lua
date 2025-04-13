4973230287d8287687c1fb8928bb5ebe53232ba5
sztorma <sztorma@outlook.com>
2025-04-13 11:09:15 +0200

    todo

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -22,4 +22,11 @@ return {
 		config = true,
 		opts = {},
 	},
+	{
+		-- Highlight todo, notes, etc in comments
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
 }
