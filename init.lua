c8e711f1bba546b925543ea6442381480a97410d
sztorma <sztorma@outlook.com>
2025-05-04 12:11:04 +0200

    lazy comments

--- init.lua
+++ init.lua
@@ -1,15 +1,28 @@
 require 'core.options'
 require 'core.keymaps'
 
+-- Declare the path where layz will clone plugin code
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+
+-- Check to see if lazy itself has been cloned, in not clone it inot the lazy.nim directory
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  local out = vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    '--branch=stable',
+    lazyrepo,
+    lazypath
+  }
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
   end
 end
+
+-- add the path to the lazy plugin repositories to the vim runtime path
 vim.opt.rtp:prepend(lazypath)
+
 require('lazy').setup({
     require 'plugins.neotree',
     require 'plugins.colortheme',
