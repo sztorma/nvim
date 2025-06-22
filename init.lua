7e935cf4b8bab6387827de23e7e7137daf715a9e
sztorma <sztorma@outlook.com>
2025-06-22 15:54:12 +0200

    nvim dap config

--- init.lua
+++ init.lua
@@ -37,5 +37,6 @@ require('lazy').setup({
     require 'plugins.gitsigns',
     require 'plugins.alpha',
     require 'plugins.indent-blankline',
-    require 'plugins.misc'
+    require 'plugins.misc',
+    require 'plugins.nvim_dap'
 })
